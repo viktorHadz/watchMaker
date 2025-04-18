44fe43a53daf0d62e036453c29315ffafed7e2aa
Viktor Hadzhiyski <130718805+viktorHadz@users.noreply.github.com>
2025-04-18 23:56:49 +0000

    animation on darkmode using component plus transition

---
+++ src/components/DarkMode.vue
@@ -0,0 +1,61 @@
+<script setup>
+import { SunIcon, MoonIcon } from '@heroicons/vue/24/outline'
+import { useColorMode } from '@vueuse/core'
+
+const mode = useColorMode({
+  attribute: 'data-theme',
+  modes: {
+    light: 'light',
+    dark: 'dark',
+  },
+  disableTransition: false,
+})
+
+const toggleMode = () => {
+  mode.value = mode.value === 'dark' ? 'light' : 'dark'
+}
+</script>
+
+<template>
+  <button @click="toggleMode" class="cursor-pointer">
+    <transition name="rotate-element" mode="out-in">
+      <div :key="mode" class="icon-wrapper">
+        <component :is="mode === 'dark' ? SunIcon : MoonIcon" class="w-8 h-8 stroke-1" />
+      </div>
+    </transition>
+  </button>
+</template>
+
+<style scoped>
+.icon-wrapper {
+  display: inline-block;
+}
+
+.rotate-element-enter-active,
+.rotate-element-leave-active {
+  transition:
+    transform 0.5s ease,
+    opacity 0.5s ease;
+  transform-origin: center center;
+}
+
+.rotate-element-enter-from {
+  opacity: 0;
+  transform: rotate(-180deg);
+}
+
+.rotate-element-enter-to {
+  opacity: 1;
+  transform: rotate(0deg);
+}
+
+.rotate-element-leave-from {
+  opacity: 1;
+  transform: rotate(0deg);
+}
+
+.rotate-element-leave-to {
+  opacity: 0;
+  transform: rotate(180deg);
+}
+</style>
