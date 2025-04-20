07180b7f0427d3bd90a32b4ed651d5ddfb4caf6a
Viktor Hadzhiyski <130718805+viktorHadz@users.noreply.github.com>
2025-04-20 21:08:58 +0000

    logo & text

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,14 @@
-<script setup></script>
+<script setup>
+import TheLogo from '@/components/TheLogo.vue'
+</script>
 
 <template>
-  <main>
-    
+  <main class="flex items-center flex-col">
+    <!-- Title -->
+    <h1 class="font-serif text-2xl">Veselin Hadzhiyski</h1>
+    <!-- Logo -->
+    <TheLogo class="size-60"></TheLogo>
+    <!-- Title Text -->
+    <h1 class="font-serif text-2xl">Watch Maker and Repairs</h1>
   </main>
 </template>
