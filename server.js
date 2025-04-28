a6d2823aa91b9e6815fb84627ae8f0e3117c9428
drslowdeath <vikecah@gmail.com>
2025-04-28 01:13:56 +0100

    start: server transfer. Too sleepy good night

--- server.js
+++ server.js
@@ -8,21 +8,9 @@
  */
 import express from 'express'
 const app = express()
-const router = express.Router()
 
 
 
-// all requests to router will first hit this middleware
-router.use((req, res, next) => {
-  console.log('%s %s %s', req.method, req.url, req.path)
-  next()
-})
-// Next router should then allow the frontend to sen informationg and call next 
-// Then the next router should receive and validate the data 
-router.get('/api/form/:data', (req, res) => {
-  // const formData = req.params
-  res.send(`Sending data: ${req.params.data}`)
-})
 
 app.use('/', express.static('dist'))
 app.get('/', (req, res) => {
